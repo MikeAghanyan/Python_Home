2d9581034c9e6242842f1a9ae70e3d65d62b0345
MikeAghanyan <mikeaghanyan@gmail.com>
2022-12-15 22:53:40 +0400

    Version4

--- less_17_def_.py
+++ less_17_def_.py
@@ -5,10 +5,30 @@
 
 # x = {2, 3, 5, 7, 34,2 ,5,10 }
 # print(x)
-
+# ------------------------------------------
+'''slide-CALCULATOR'''
 # from lib import calculator
-# print(calculator(7, 65, '*'))
+# print(calculator(5, 5, '*'))
 
+# ------------------------------------------------------
+'''To find max of two numbers'''
+# print(max(int(input('Enter a number ---> ')), int(input('Enter a number ---> '))))
+
+# ------------------------------------------------------
+'''SUM'''
+# def summ(*x):
+
+
+def summ(*x):
+    a = 0
+    while x != 0:
+        x = int(input('Enter a number ---> '))
+        a += x
+    return a
+print(summ())
+
+# ------------------------------------------------------
+'''BOOOK ---> '''
 # def item(a) -> float:
 #     x = 10.95
 #     if a == 1:
@@ -20,4 +40,8 @@
 #         return 'ERROR'
 
 # print(item(3))
-          
+
+
+
+
+
--- lib.py
+++ lib.py
@@ -1,23 +1,27 @@
-# def tri(x, y, z):
-#     a = x + y + z
-#     return a
+def tri(x, y, z):
+    a = x + y + z
+    return a
 
-# def calculator(a:float, b:float, operator:str) -> float:
-#     if operator == '+':
-#         return a + b
-#     elif operator == '+':
-#         return a - b
-#     elif operator == '/':
-#         return a / b
-#     elif operator == '*':
-#         return a * b
-#     elif operator == '**':
-#         return a ** b
-#     elif operator == 'log':
-#         return __import__('math').log(a, b)
-#     else:
-#         return 'ERROR'
+'''slide-CALCULATOR'''
+def calculator(a:float, b:float, operator:str) -> float:
+    if operator == '+':
+        return a + b
+    elif operator == '+':
+        return a - b
+    elif operator == '/':
+        return a / b
+    elif operator == '*':
+        return a * b
+    elif operator == '**':
+        return a ** b
+    elif operator == 'log':
+        return __import__('math').log(a, b)
+    else:
+        return 'ERROR'
         # rise ValueError
-    
+
+'''To find max of two numbers'''
+
+
 
     
